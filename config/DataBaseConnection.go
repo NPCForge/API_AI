e62527d51890136b9c2a59f1125da240184a71bb
Grainbox <tagemania@gmail.com>
2024-12-16 15:19:18 +0900

    [Feat]: Is running in docker detection to set DB host + retry db connection 4 times

--- config/DataBaseConnection.go
+++ config/DataBaseConnection.go
@@ -1,10 +1,14 @@
 package config
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"sync"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -14,30 +18,72 @@ var (
 	once     sync.Once
 )
 
+func IsRunningInDocker() bool {
+	file, err := os.Open("/proc/1/cgroup")
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "/docker/") || strings.Contains(scanner.Text(), "/kubepods/") {
+			return true
+		}
+	}
+	return false
+}
+
 func InitDB() {
 	once.Do(func() {
 		// Configuration de la connexion
 		connStr := "user=" + GetEnvVariable("POSTGRES_USER")
 		connStr += " password=" + GetEnvVariable("POSTGRES_PASSWORD")
 		connStr += " dbname=" + GetEnvVariable("POSTGRES_DB")
-		connStr += " host=" + GetEnvVariable("POSTGRES_HOST")
+
+		if IsRunningInDocker() {
+			connStr += " host=postgres"
+		} else {
+			connStr += " host=" + GetEnvVariable("POSTGRES_HOST")
+		}
 		connStr += " port=" + GetEnvVariable("POSTGRES_PORT")
 		connStr += " sslmode=disable"
 
-		// Se connecter à la base de données
 		var err error
-		dbClient, err = sql.Open("postgres", connStr)
-		if err != nil {
-			log.Fatalf("Erreur lors de l'ouverture de la connexion à PostgreSQL : %v", err)
+		maxRetries := 4
+		retryDelay := 10 * time.Second
+
+		// Réessayer de se connecter plusieurs fois
+		for retries := 0; retries < maxRetries; retries++ {
+			// Tenter d'ouvrir la connexion
+			dbClient, err = sql.Open("postgres", connStr)
+			if err != nil {
+				log.Printf("Erreur lors de l'ouverture de la connexion à PostgreSQL (tentative %d/%d) : %v", retries+1, maxRetries, err)
+			} else {
+				// Tester la connexion
+				err = dbClient.Ping()
+				if err == nil {
+					fmt.Println("Connexion à PostgreSQL réussie !")
+					return
+				}
+
+				log.Printf("Erreur de connexion à PostgreSQL (tentative %d/%d) : %v", retries+1, maxRetries, err)
+			}
+
+			// Fermer la connexion en cas d'erreur pour éviter les fuites
+			if dbClient != nil {
+				dbClient.Close()
+			}
+
+			// Attendre avant la prochaine tentative
+			if retries < maxRetries-1 {
+				log.Printf("Nouvelle tentative dans %v...", retryDelay)
+				time.Sleep(retryDelay)
+			}
 		}
 
-		// Tester la connexion
-		err = dbClient.Ping()
-		if err != nil {
-			log.Fatalf("Erreur de connexion à PostgreSQL : %v", err)
-		}
-
-		fmt.Println("Connexion à PostgreSQL réussie !")
+		// Si toutes les tentatives échouent
+		log.Fatalf("Échec de connexion à PostgreSQL après %d tentatives : %v", maxRetries, err)
 	})
 }
 
