2d356eea7912695727bdf3b545bd367a2b17bea3
“Mathieu” <mathieu.rio@epitech.eu>
2025-05-02 22:48:02 +0200

    feat: add components

--- UI/components/ButtonGroup.vue
+++ UI/components/ButtonGroup.vue
@@ -1,34 +1,48 @@
 <template>
-    <div class="btn-group " v-for="(button, index) in buttons" :key="index" style="width: 100%">
+    <div class="btn-group dropend" v-for="(button, index) in buttons" :key="index" style="width: 100%">
         <!-- Vérifier si le bouton a un menu déroulant -->
-        <button v-if="button.dropdown" type="button" class="dropdown-toggle btn btn-primary" data-bs-toggle="dropdown"
-            aria-expanded="false" style="width: 100%; height: 5vh; border: 0;" @click="button.onClick">
-            <!-- Lier la fonction onClick -->
+        <button v-if="button.dropdown" type="button" class="dropdown-toggle" data-bs-toggle="dropdown"
+            aria-expanded="false" style="width: 100%; height: 5vh; border: 0;">
             {{ button.label }}
         </button>
-        <button v-else type="button" class="btn btn-primary" style="width: 100%; height: 5vh; border: 0;"
-            @click="button.onClick"> <!-- Lier la fonction onClick -->
+        <button v-else type="button" class="" style="width: 100%; height: 5vh; border: 0;"
+            @click="change(button.label)">
             {{ button.label }}
         </button>
 
         <!-- Dropdown menu pour les boutons ayant une option de menu -->
         <ul v-if="button.dropdown" class="dropdown-menu">
             <li v-for="(item, index) in button.menuItems" :key="index">
-                <a class="dropdown-item" href="#">{{ item }}</a>
+                <!-- Lier le clic de l'élément du menu avec une fonction change -->
+                <a class="dropdown-item" href="#" @click="change(`${button.label}_${item}`)">
+                    {{ item }}
+                </a>
             </li>
         </ul>
     </div>
 </template>
 
 <script setup>
-defineProps({
-    buttons: {
-        type: Array,
-        required: true
+    defineProps({
+        buttons: {
+            type: Array,
+            required: true
+        }
+    })
+
+    // Déclare l'événement 'change' que nous allons émettre
+    const emit = defineEmits()
+
+    // Fonction de gestion du clic sur un bouton ou un élément de menu
+    const change = (name) => {
+        // Émettre l'événement 'change' avec le nom nettoyé de tout espace
+        emit('change', name.replace(/\s+/g, '')) // Remplacer tous les espaces
     }
-})
 </script>
 
 <style scoped>
-/* Style du bouton et du menu déroulant */
+    /* Style du bouton et du menu déroulant */
+    button:hover {
+        background-color: rgb(211, 211, 211);
+    }
 </style>
--- UI/pages/Secure/index.vue
+++ UI/pages/Secure/index.vue
@@ -1,9 +1,26 @@
 <template>
     <div class="d-flex justify-content-start align-items-start vh-100">
-        <div class="d-flex flex-column justify-content-start align-items-start vw-15 vh-100 blue">
-            <ButtonGroup :buttons="buttons" />
+        <div class="d-flex flex-column justify-content-start align-items-start vw-15 vh-100">
+            <ButtonGroup :buttons="buttons" @change="changePage"/>
         </div>
-        <div class="d-flex justify-content-start align-items-start vw-100 vh-100"></div>
+        <div class="d-flex justify-content-start align-items-start vw-100 vh-100 red">
+            <Settings
+                v-if="currentPage === 'Settings'"
+                :pageName="'Settings'"
+            />
+            <EntitiesManager
+                v-if="currentPage === 'EntitiesManager'"
+                :pageName="'Entities Manager'"
+            />
+            <PromptsManager
+                v-if="currentPage === 'PromptsManager'"
+                :pageName="'Prompts Manager'"
+            />
+            <UsersManager
+                v-if="currentPage === 'UsersManager'"
+                :pageName="'Users Manager'"
+            />
+        </div>
     </div>
 </template>
 
@@ -11,37 +28,42 @@
 import { ref } from 'vue'
 import ButtonGroup from '~/components/ButtonGroup.vue'
 
+import Settings from "~/components/features/Settings.vue"
+import EntitiesManager from "~/components/features/EntitiesManager.vue"
+import PromptsManager from "~/components/features/PromptsManager.vue"
+import UsersManager from "~/components/features/UsersManager.vue"
+
+const currentPage = ref("")
+
+const changePage = (page) => {
+    currentPage.value = page
+}
+
 const buttons = ref([
     {
         label: 'Disconnect',
         dropdown: false,
-        onClick: () => { console.log('Button 1 clicked') }  // Fonction associée à Button 1
     },
     {
         label: 'Simulate',
         dropdown: true,
-        menuItems: ['Action', 'Another action', 'Something else'],
-        onClick: () => { console.log('Button 2 clicked') }  // Fonction associée à Button 2
+        menuItems: ['Connect', 'Register', 'Disconnect', 'RemoveUser', 'CreateEntity', 'RemoveEntity', 'GetEntities', 'NewMessage', 'MakeDecision'],
     },
     {
-        label: 'Entities manager',
+        label: 'Entities Manager',
         dropdown: false,
-        onClick: () => { console.log('Button 1 clicked') }  // Fonction associée à Button 1
     },
     {
-        label: 'Users manager',
+        label: 'Users Manager',
         dropdown: false,
-        onClick: () => { console.log('Button 1 clicked') }  // Fonction associée à Button 1
     },
     {
-        label: 'Prompt manager',
+        label: 'Prompts Manager',
         dropdown: false,
-        onClick: () => { console.log('Button 1 clicked') }  // Fonction associée à Button 1
     },
     {
         label: 'Settings',
         dropdown: false,
-        onClick: () => { console.log('Button 1 clicked') }  // Fonction associée à Button 1
     },
 ])
 
