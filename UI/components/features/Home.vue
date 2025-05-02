39e69cf0d229ebaa718ddccfa309030a894b8750
“Mathieu” <mathieu.rio@epitech.eu>
2025-05-02 23:08:20 +0200

    feat: add home page

---
+++ UI/components/features/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="body">
+        <h3>{{ pageName }}</h3>
+        
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        pageName: String
+    })
+</script>
+
+<style scoped>
+    .body {
+        height: 100%;
+        width: 100%;
+        overflow-y: scroll;
+        overflow-x: hidden;
+    }
+</style>
--- UI/pages/Secure/index.vue
+++ UI/pages/Secure/index.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="d-flex justify-content-start align-items-start vh-100">
-        <div class="d-flex flex-column justify-content-start align-items-start vw-15 vh-100">
+        <div class="d-flex flex-column justify-content-start align-items-start vw-15 vh-100 leftBar">
             <ButtonGroup :buttons="buttons" @change="changePage"/>
         </div>
-        <div class="d-flex justify-content-start align-items-start vw-100 vh-100 red">
+        <div class="d-flex justify-content-start align-items-start vw-100 vh-100" style="padding: 0.5%;">
+            <Home
+                v-if="currentPage === 'Home'"
+                :pageName="'Home'"
+            />
             <Settings
                 v-if="currentPage === 'Settings'"
                 :pageName="'Settings'"
@@ -28,12 +32,13 @@
 import { ref } from 'vue'
 import ButtonGroup from '~/components/ButtonGroup.vue'
 
+import Home from "~/components/features/Home.vue"
 import Settings from "~/components/features/Settings.vue"
 import EntitiesManager from "~/components/features/EntitiesManager.vue"
 import PromptsManager from "~/components/features/PromptsManager.vue"
 import UsersManager from "~/components/features/UsersManager.vue"
 
-const currentPage = ref("")
+const currentPage = ref("Home")
 
 const changePage = (page) => {
     currentPage.value = page
@@ -41,7 +46,7 @@ const changePage = (page) => {
 
 const buttons = ref([
     {
-        label: 'Disconnect',
+        label: 'Home',
         dropdown: false,
     },
     {
@@ -65,6 +70,17 @@ const buttons = ref([
         label: 'Settings',
         dropdown: false,
     },
+    {
+        label: 'Disconnect',
+        dropdown: false,
+    },
 ])
 
 </script>
+
+<style scoped>
+    .leftBar {
+        background-color: rgb(242, 242, 242);
+        border-right: 3px solid rgb(207, 207, 207);
+    }
+</style>
